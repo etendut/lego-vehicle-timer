ffccb754f828b8f584849ff5837581ac4d3e232a
etendut <etendut@gmail.com>
2025-10-11 10:07:45 +0800

    odv fixes

--- modules/vehicle_odv.py
+++ modules/vehicle_odv.py
@@ -28,8 +28,8 @@ ODV_SPEED: int = const(45)  # set between 40 and 70
 # ODV_GRID = ["XL##XU", "H#X###"]
 
 ODV_GRID_YE2 = ["XH<U", "X#X#", "L###"]
-ODV_GRID_GR3 = ["L##<U", "X#XH#", "X###X"]
-ODV_GRID_BL4 = ["H###X", "X#X#X", "L#<#U"]
+ODV_GRID_GR3 = ["L##<U", "H#X#X", "X###X"]
+ODV_GRID_BL4 = ["X###X", "#X#X", "L#<#U"]
 
 ODV_GRID = ODV_GRID_GR3
 
