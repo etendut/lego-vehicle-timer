7d51dc2fd6d7a102c56e60e720ba43ac002e1e51
etendut <etendut@gmail.com>
2026-04-18 06:49:49 +0800

    enable remote

--- docs/pybricks/lego_vehicle_timer_odv.py
+++ docs/pybricks/lego_vehicle_timer_odv.py
@@ -44,7 +44,7 @@ COUNTDOWN_RESET_CODE = 'c,c,c'  # left center button, center button, right cente
 ODV_AUTO_DRIVE_TIMEOUT_SECS: int = const(0)
 
 # for debugging or ODV full auto
-REMOTE_DISABLED = True
+REMOTE_DISABLED = False
 
 # low voltage protection in millivolts e.g. 1.2 * 6 * 1000 = 7200mV
 MILLIVOLT_CRITICAL_LEVEL = const(7200) 
