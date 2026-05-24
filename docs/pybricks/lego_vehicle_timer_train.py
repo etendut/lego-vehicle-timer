c8237ce9c17c301f343b1897f36f190065ec3211
Etendut <etendut@gmail.com>
2026-05-24 11:15:38 +0800

    recomp

--- docs/pybricks/lego_vehicle_timer_train.py
+++ docs/pybricks/lego_vehicle_timer_train.py
@@ -22,7 +22,7 @@ from pybricks.pupdevices import DCMotor, Light
 
 
 
-__BUILD__ = '3a55f51'  # replaced at compile time with git hash + timestamp
+__BUILD__ = 'f8046b6'  # replaced at compile time with git hash + timestamp
 print('Version 3.0.0 build', __BUILD__)
 ##################################################################################
 #  Settings
