a67d3483f0d3ccd7aeac3f55a7c9d0af26af4263
Etendut <etendut@gmail.com>
2026-05-24 16:32:47 +0800

    recomp

--- docs/pybricks/lego_vehicle_timer_skid_steer.py
+++ docs/pybricks/lego_vehicle_timer_skid_steer.py
@@ -23,7 +23,7 @@ from uerrno import ENODEV
 
 
 
-__BUILD__ = '05fd1c6'  # replaced at compile time with git hash + timestamp
+__BUILD__ = '502a7aa'  # replaced at compile time with git hash + timestamp
 print('Version 3.0.0 build', __BUILD__)
 ##################################################################################
 #  Settings
--- docs/pybricks/lego_vehicle_timer_train.py
+++ docs/pybricks/lego_vehicle_timer_train.py
@@ -22,7 +22,7 @@ from pybricks.pupdevices import DCMotor, Light
 
 
 
-__BUILD__ = '05fd1c6'  # replaced at compile time with git hash + timestamp
+__BUILD__ = '502a7aa'  # replaced at compile time with git hash + timestamp
 print('Version 3.0.0 build', __BUILD__)
 ##################################################################################
 #  Settings
