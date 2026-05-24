fe0cb195e47f7804dfa61d127789e9b1b24fd501
Etendut <etendut@gmail.com>
2026-05-24 16:14:57 +0800

    recomp

--- docs/pybricks/lego_vehicle_timer_odv.py
+++ docs/pybricks/lego_vehicle_timer_odv.py
@@ -32,7 +32,7 @@ except ImportError:
     ENODEV = -99
 
 
-__BUILD__ = '9896f16'  # replaced at compile time with git hash + timestamp
+__BUILD__ = '4cb257c'  # replaced at compile time with git hash + timestamp
 print('Version 3.0.0 build', __BUILD__)
 ##################################################################################
 #  Settings
--- docs/pybricks/lego_vehicle_timer_servo.py
+++ docs/pybricks/lego_vehicle_timer_servo.py
@@ -23,7 +23,7 @@ from uerrno import ENODEV
 
 
 
-__BUILD__ = '9896f16'  # replaced at compile time with git hash + timestamp
+__BUILD__ = '4cb257c'  # replaced at compile time with git hash + timestamp
 print('Version 3.0.0 build', __BUILD__)
 ##################################################################################
 #  Settings
